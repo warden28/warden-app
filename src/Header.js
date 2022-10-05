07393f726b35576500f3fd828d41e55a94955cc3
warden28 <99381658+warden28@users.noreply.github.com>
2022-10-05 23:44:17 +0700

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,24 +7,24 @@ const Header = () => {
       {/* navbar  */}
       <nav className="navbar navbar-primary bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand text-light" href="#">
+          <a className="navbar-brand text-light">
             Warden
           </a>
         </div>
       </nav>
-      <div className="fixed-bottom">
-        <a href=
-          'https://api.whatsapp.com/send?phone=6289509830790&text=Halo%20Admin%20Saya%20Mau%20Order'
-        >
-          <img src={wa} alt="logo"
-            style={{
-              float: 'right',
-              width: '80px',
-              marginRight: '20px',
-              marginBottom: '30px'
-            }}
-          />
-        </a>
+//       <div className="fixed-bottom">
+//         <a href=
+//           'https://api.whatsapp.com/send?phone=6289509830790&text=Halo%20Admin%20Saya%20Mau%20Order'
+//         >
+//           <img src={wa} alt="logo"
+//             style={{
+//               float: 'right',
+//               width: '80px',
+//               marginRight: '20px',
+//               marginBottom: '30px'
+//             }}
+//           />
+//         </a>
 
       </div>
     </div >
